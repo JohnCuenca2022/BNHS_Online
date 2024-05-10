123d19e116d7a99d5c847ec1819612d796e9ef90
marthabacoy <marymartha.bacoy.cics@ust.edu.ph>
2024-05-11 05:53:02 +0800

    admin dash

---
+++ src/views/admin/AdminDash.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <h2>Create Announcement</h2>
+      <form @submit.prevent="submitAnnouncement">
+        <div class="form-group">
+          <label for="title">Title:</label>
+          <input type="text" id="title" v-model="title" required>
+        </div>
+        <div class="form-group">
+          <label for="content">Content:</label>
+          <textarea id="content" v-model="content" required></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        title: '',
+        content: ''
+      };
+    },
+    methods: {
+      submitAnnouncement() {
+        this.$emit('new-announcement', {
+          title: this.title,
+          content: this.content
+        });
+        this.title = '';
+        this.content = '';
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .form-group {
+    margin-bottom: 1rem;
+  }
+  </style>
+  
